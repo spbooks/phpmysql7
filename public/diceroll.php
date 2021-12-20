3ffa75bb0cecc6230e100a11e2e78f4990965365
Tom Butler <tom@r.je>
2021-12-20 16:50:00 +0000

    PHP-DiceRoll-TwoDice

--- public/diceroll.php
+++ public/diceroll.php
@@ -1,7 +1,14 @@
 <?php 
-if ($roll > 4) {
+$roll1 = rand(1, 6);
+$roll2 = rand(1, 6);
+
+echo '<p>You rolled a ' . $roll1 . ' and a ' . $roll2 . '</p>';
+
+if ($roll1 == 6 && $roll2 == 6) {
   echo '<p>You win!</p>';
 }
 else {
   echo '<p>Sorry, you didn\'t win. Better luck next time!</p>';
 }
+
+echo '<p>Thanks for playing</p>';
