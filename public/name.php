20bb961c62a986d59d5ccf08f09165f021104a7a
Tom Butler <tom@r.je>
2021-12-20 17:09:10 +0000

    PHP-Count-Template

--- public/name.php
+++
@@ -1,7 +0,0 @@
-<?php
-$firstname = $_POST['firstname'];
-$lastname = $_POST['lastname'];
-echo 'Welcome to our website, ' .
-  htmlspecialchars($firstname, ENT_QUOTES, 'UTF-8') . ' ' .
-  htmlspecialchars($lastname, ENT_QUOTES, 'UTF-8') . '!';
-?>
