3312b104e055495f5470eac3bbbeb59276a2c9fe
Tom Butler <tom@r.je>
2021-12-20 17:05:17 +0000

    PHP-GET-TwoVars

--- public/name.php
+++ public/name.php
@@ -1,5 +1,7 @@
 <?php
-$name = $_GET['name'];
+$firstName = $_GET['firstname'];
+$lastName = $_GET['lastname'];
 echo 'Welcome to our website, ' .
-  htmlspecialchars($name, ENT_QUOTES, 'UTF-8') . '!';
+  htmlspecialchars($firstName, ENT_QUOTES, 'UTF-8') . ' ' .
+  htmlspecialchars($lastName, ENT_QUOTES, 'UTF-8') . '!';
 ?>
