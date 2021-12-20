9fe053b2f152cf39f528de8dbb6073a077be8944
Tom Butler <tom@r.je>
2021-12-20 16:49:05 +0000

    PHP-DiceRoll-Or

--- public/diceroll.php
+++ public/diceroll.php
@@ -1,13 +1,7 @@
 <?php 
-$roll = rand(1, 6);
-
-echo '<p>You rolled a ' . $roll . '</p>';
-
-if ($roll == 6) {
+if ($roll == 6 || $roll == 5) {
   echo '<p>You win!</p>';
 }
 else {
   echo '<p>Sorry, you didn\'t win. Better luck next time!</p>';
 }
-
-echo '<p>Thanks for playing</p>';
