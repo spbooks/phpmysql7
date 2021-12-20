138f79a95d74d50a287dc4e8f5194644ae0e685e
Tom Butler <tom@r.je>
2021-12-20 16:52:40 +0000

    PHP-DiceRoll-ManyDice

--- public/diceroll.php
+++ public/diceroll.php
@@ -1,8 +1,14 @@
 <?php 
-$roll = rand(1, 6);
+for ($count = 1; $count <= 10; $count++) {
+  $roll = rand(1, 6);
+  echo '<p>You rolled a ' . $roll . '</p>';
 
-echo 'You rolled a ' . $roll;
-
-if ($roll == 6) {
-  echo 'You win!';
+  if ($roll == 6) {
+    echo '<p>You win!</p>';
+  }
+  else {
+    echo '<p>Sorry, you didn\'t win. Better luck next time!</p>';
+  }
 }
+
+echo '<p>Thanks for playing</p>';
