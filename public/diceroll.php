7f0033dbcd2b1c42769d6ff2bf29d58e53e2ab87
Tom Butler <tom@r.je>
2021-12-20 16:47:48 +0000

    PHP-DiceRoll-Else

--- public/diceroll.php
+++ public/diceroll.php
@@ -6,5 +6,8 @@ echo '<p>You rolled a ' . $roll . '</p>';
 if ($roll == 6) {
   echo '<p>You win!</p>';
 }
+else {
+  echo '<p>Sorry, you didn\'t win. Better luck next time!</p>';
+}
 
 echo '<p>Thanks for playing</p>';
