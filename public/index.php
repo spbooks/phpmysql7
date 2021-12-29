75fba809ed97679f758d31222a800fd894923e53
Tom Butler <tom@r.je>
2021-12-29 14:30:17 +0000

    MySQL-ListJokes

--- public/index.php
+++ public/index.php
@@ -1,17 +1,16 @@
 <?php
 try {
-  $pdo = new PDO('mysql:host=mysql;dbname=ijdb;charset=utf8mb4', 'ijdbuser', 'mypassword');
+    $pdo = new PDO('mysql:host=mysql;dbname=ijdb;charset=utf8mb4', 'ijdbuser', 'mypassword');
 
-  $sql = 'UPDATE joke SET jokedate="2021-04-01"
-      WHERE joketext LIKE "%programmer%"';
+    $sql = 'SELECT `joketext` FROM `joke`';
+    $result = $pdo->query($sql);
 
-  $affectedRows = $pdo->exec($sql);
-
-  $output = 'Updated ' . $affectedRows .' rows.';
-}
-catch (PDOException $e) {
-  $output = 'Database error: ' . $e->getMessage() . ' in ' .
+    while ($row = $result->fetch()) {
+        $jokes[] = $row['joketext'];
+    }
+} catch (PDOException $e) {
+    $error = 'Unable to connect to the database server: ' . $e->getMessage() . ' in ' .
   $e->getFile() . ':' . $e->getLine();
 }
 
-include  __DIR__ . '/../templates/output.html.php';
+include  __DIR__ . '/../templates/jokes.html.php';
