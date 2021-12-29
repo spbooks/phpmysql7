1e3dc9cecce7ae559b1923aeaccff9e9add69292
Tom Butler <tom@r.je>
2021-12-29 16:07:26 +0000

    SaveArray

--- public/editjoke.php
+++ public/editjoke.php
@@ -1,33 +1,34 @@
 <?php
+include __DIR__ . '/../includes/DatabaseConnection.php';
+include __DIR__ . '/../includes/DatabaseFunctions.php';
+
 try {
-    include __DIR__ . '/../includes/DatabaseConnection.php';
-    include __DIR__ . '/../includes/DatabaseFunctions.php';
+        if (isset($_POST['joke'])) {
+                $joke = $_POST['joke'];
+                $joke['jokedate'] = new DateTime();
+                $joke['authorId'] = 1;
 
-    if (isset($_POST['joketext'])) {
-        update($pdo, 'joke', 'id', [
-                     'id' => $_POST['jokeid'],
-                     'joketext' => $_POST['joketext'],
-                     'authorId' => 1
-             ]
-        );
+                save($pdo, 'joke', 'id', $joke);
 
-        header('location: jokes.php');
-    } else {
-        $joke = find($pdo, 'joke', 'id', $_GET['id'])[0];
+                header('location: jokes.php');
+        } else {
+                if (isset($_GET['id'])) {
+                        $joke = find($pdo, 'joke', 'id', $_GET['id'])[0] ?? null;
+                }
 
-        $title = 'Edit joke';
+                $title = 'Edit joke';
 
-        ob_start();
+                ob_start();
 
-        include  __DIR__ . '/../templates/editjoke.html.php';
+                include  __DIR__ . '/../templates/editjoke.html.php';
 
-        $output = ob_get_clean();
-    }
+                $output = ob_get_clean();
+        }
 } catch (PDOException $e) {
-    $title = 'An error has occurred';
+        $title = 'An error has occurred';
 
-    $output = 'Database error: ' . $e->getMessage() . ' in ' .
-    $e->getFile() . ':' . $e->getLine();
+        $output = 'Database error: ' . $e->getMessage() . ' in ' .
+        $e->getFile() . ':' . $e->getLine();
 }
 
 include  __DIR__ . '/../templates/layout.html.php';
--- templates/editjoke.html.php
+++ templates/editjoke.html.php
@@ -1,6 +1,6 @@
 <form action="" method="post">
-  <input type="hidden" name="jokeid" value="<?=$joke['id'];?>">
-  <label for="joketext">Type your joke here:</label>
-  <textarea id="joketext" name="joketext" rows="3" cols="40"><?=htmlspecialchars($joke['joketext'], ENT_QUOTES, 'UTF-8')?></textarea>
-  <input type="submit" value="Save">
+  <input type="hidden" name="joke[id]" value="<?=$joke['id'] ?? ''?>">
+    <label for="joketext">Type your joke here:</label>
+    <textarea id="joketext" name="joke[joketext]" rows="3" cols="40"><?=$joke['joketext'] ?? ''?></textarea>
+    <input type="submit" name="submit" value="Save">
 </form>
