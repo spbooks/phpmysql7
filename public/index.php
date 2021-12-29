6ff8d8caa4a2c1cbb816fa05843f53562f46d07b
Tom Butler <tom@r.je>
2021-12-29 14:26:09 +0000

    MySQL-Create

--- public/index.php
+++ public/index.php
@@ -1,9 +1,20 @@
 <?php
 try {
-    $pdo = new PDO('mysql:host=mysql;dbname=ijdb;charset=utf8mb4', 'ijdbuser', 'mypassword');
-    $output = 'Database connection established.';
-} catch (PDOException $e) {
-    $output = 'Unable to connect to the database server: ' . $e->getMessage();
+  $pdo = new PDO('mysql:host=mysql;dbname=ijdb;charset=utf8mb4', 'ijdbuser', 'mypassword');
+
+  $sql = 'CREATE TABLE joke (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    joketext TEXT,
+    jokedate DATE NOT NULL
+  ) DEFAULT CHARACTER SET utf8 ENGINE=InnoDB';
+
+  $pdo->exec($sql);
+
+  $output = 'Joke table successfully created.';
+}
+catch (PDOException $e) {
+  $output = 'Database error: ' . $e->getMessage() . ' in ' .
+  $e->getFile() . ':' . $e->getLine();
 }
 
 include  __DIR__ . '/../templates/output.html.php';
