7a8ea35c6c3ed3dd09eaf56ee167a9d91ac57253
Tom Butler <tom@r.je>
2021-12-29 14:22:13 +0000

    try {     $pdo = new PDO('mysql:host=mysql;dbname=ijdb;charset=utf8mb4', 'ijdbuser', 'mypassword');     $output = 'Database connection established.'; } catch (PDOException $e) {     $output = 'Unable to connect to the database server: ' . $e->getMessage(); }
    include  __DIR__ . '/../templates/output.html.php';

--- public/index.php
+++ public/index.php
@@ -1,11 +1,9 @@
 <?php
 try {
-  $pdo = new PDO('mysql:host=mysql;dbname=ijdb', 'ijdbuser',
-    'mypassword');
-   $output = 'Database connection established.';
-}
-catch (PDOException $e) {
-  $output = 'Unable to connect to the database server: ' . $e->getMessage();
+    $pdo = new PDO('mysql:host=mysql;dbname=ijdb;charset=utf8mb4', 'ijdbuser', 'mypassword');
+    $output = 'Database connection established.';
+} catch (PDOException $e) {
+    $output = 'Unable to connect to the database server: ' . $e->getMessage();
 }
 
 include  __DIR__ . '/../templates/output.html.php';
