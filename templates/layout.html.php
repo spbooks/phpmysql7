ef2aae12f8712642b8b8eb2a2fb0d2682abd9341
Tom Butler <tom@r.je>
2022-01-02 15:47:32 +0000

    update layout links

--- templates/layout.html.php
+++ templates/layout.html.php
@@ -12,8 +12,8 @@
     </header>
     <ul>
       <li><a href="index.php">Home</a></li>
-      <li><a href="index.php?action=list">Jokes List</a></li>
-      <li><a href="index.php?action=edit">Add a new Joke</a></li>
+    <li><a href="index.php?controller=joke&amp;action=list">Jokes List</a></li>
+    <li><a href="index.php?controller=joke&amp;action=edit">Add a new Joke</a></li>
     </ul>
   </nav>
 
