51fead2b56162888998f9238f88063a659d1fee0
Tom Butler <tom@r.je>
2022-01-02 16:55:24 +0000

    remove code from next example

--- Ijdb/Controllers/Author.php
+++ Ijdb/Controllers/Author.php
@@ -1,29 +1,11 @@
 <?php
 namespace Ijdb\Controllers;
-
-use \Ninja\DatabaseTable;
-
 class Author {
-    public function __construct(private DatabaseTable $authorsTable) {
-    }
+	public function __construct(private \Ninja\DatabaseTable $authorsTable) {
 
-    public function registrationForm() {
-        return [
-          'template' => 'register.html.php',
-            'title' => 'Register an account'
-         ];
-    }
-
-    public function registrationFormSubmit() {
-	  $author = $_POST['author'];
-
-	  $this->authorsTable->save($author);
-
-	  header('Location: /author/success');
 	}
 
-    public function success() {
-        return ['template' => 'registersuccess.html.php',
-            'title' => 'Registration Successful'];
-    }
+	public function register() {
+		//sample function for a second controller
+	}
 }
--- templates/registersuccess.html.php
+++
@@ -1,3 +0,0 @@
-<h2>Registration Successful</h2>
-
-<p>You are now registered on the Internet Joke Database</p>
