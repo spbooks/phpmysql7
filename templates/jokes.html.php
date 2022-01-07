be1218a43bd92cf30bfe96d15ee68a1e87421923
Tom Butler <tom@r.je>
2022-01-07 21:16:58 +0000

    Formatting-Markdown

--- templates/jokes.html.php
+++ templates/jokes.html.php
@@ -11,8 +11,7 @@
 
 <?php foreach ($jokes as $joke): ?>
 <blockquote>
-  <p>
-  <?=htmlspecialchars($joke->joketext, ENT_QUOTES, 'UTF-8')?>
+<?=(new \Ninja\Markdown($joke->joketext))->toHtml()?></p>
 
     (by <a href="mailto:<?=htmlspecialchars($joke->getAuthor()->email, ENT_QUOTES,  'UTF-8'); ?>">
       <?=htmlspecialchars($joke->getAuthor()->name, ENT_QUOTES, 'UTF-8'); ?></a> on
@@ -34,7 +33,7 @@ echo $date->format('jS F Y');
   </form>
   <?php endif; ?>
 <?php endif; ?>
-  </p>
+
 </blockquote>
 <?php endforeach; ?>
 </div>
