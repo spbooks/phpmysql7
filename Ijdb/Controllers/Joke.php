249854d1a04bfe825c58f115b8ae24d1cae89f8f
Tom Butler <tom@r.je>
2022-01-07 17:32:51 +0000

    remove extra method

--- Ijdb/Controllers/Joke.php
+++ Ijdb/Controllers/Joke.php
@@ -46,60 +46,13 @@ class Joke {
 		    ]
 	       ];
 	}
-	public function list2() {
-	    $result = $this->jokesTable->findAll();
-
-	    $jokes = [];
-	    foreach ($result as $joke) {
-	        $author = $this->authorsTable->find('id', $joke->authorId)[0];
-
-			$jokes[] = [
-			  'id' => $joke->id,
-			  'joketext' => $joke->joketext,
-			  'jokedate' => $joke->jokedate,
-			  'name' => $author->name,
-			  'email' => $author->email,
-			  'authorId' => $joke->authorId
-			];
-
-	    }
-
-	    $title = 'Joke list';
-
-	    $totalJokes = $this->jokesTable->total();
-
-	    $user = $this->authentication->getUser();
-
-
-	    return ['template' => 'jokes.html.php',
-		    'title' => $title,
-		    'variables' => [
-		        'totalJokes' => $totalJokes,
-		        'jokes' => $jokes,
-		        'userId' => $user->id ?? null
-		    ]
-		];
-
-	}
-
+	
 	public function editSubmit() {
-		// Get the currently logged in user as the $author to associate the joke with
-		$author = $this->authentication->getUser();
+		$authorObject = $this->authentication->getUser();
 
-		// Create an `Author` entity instance
-		$authorObject = new \Ijdb\Entity\Author($this->jokesTable);
-
-		// Copy the values from the `$author` array into the corresponding properties in the entity object
-		$authorObject->id = $author['id'];
-		$authorObject->name = $author['name'];
-		$authorObject->email = $author['email'];
-		$authorObject->password = $author['password'];
-
-		// Read the form submission and set the date
 		$joke = $_POST['joke'];
 		$joke['jokedate'] = new \DateTime();
 
-		// Save the joke using the new addJoke method
 		$authorObject->addJoke($joke);
 
 		header('location: /joke/list');
