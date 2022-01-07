eed8eb9d5e765f7d1db9bd076578955f4c3928a2
Tom Butler <tom@r.je>
2022-01-07 17:22:15 +0000

    use class variables

--- Ijdb/JokeWebsite.php
+++ Ijdb/JokeWebsite.php
@@ -26,14 +26,11 @@ class JokeWebsite implements \Ninja\Website {
     public function getController(string $controllerName): ?object {
         $pdo = new \PDO('mysql:host=mysql;dbname=ijdb;charset=utf8mb4', 'ijdbuser', 'mypassword');
 
-        $jokesTable = new \Ninja\DatabaseTable($pdo, 'joke', 'id');
-        $authorsTable = new \Ninja\DatabaseTable($pdo, 'author', 'id');
-
         if ($controllerName === 'joke') {
-            $controller = new \Ijdb\Controllers\Joke($jokesTable, $authorsTable, $this->authentication);
+            $controller = new \Ijdb\Controllers\Joke($this->jokesTable, $this->authorsTable, $this->authentication);
         }
         else if ($controllerName === 'author') {
-            $controller = new \Ijdb\Controllers\Author($authorsTable);
+            $controller = new \Ijdb\Controllers\Author($this->authorsTable);
         }
         else if ($controllerName == 'login') {
             $controller = new \Ijdb\Controllers\Login($this->authentication);
